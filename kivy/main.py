735b59e42439be616b55ed37f3940708b0b0ba1c
linnil1 <linnil1.886@gmail.com>
2018-04-21 16:47:17 +0800

    use kv lang to rewrite

--- kivy/main.py
+++ kivy/main.py
@@ -4,73 +4,72 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
+from kivy.uix.widget import  Widget
 from kivy.properties import NumericProperty
 
-# from kivy.lang.builder import Builder
-# Builder.load_string(kv_string)
+from kivy.config import Config
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen 
+Config.set('kivy', 'log_level', 'debug')
+Config.write()
 
-class Count(BoxLayout):
+class Menu(Screen):
+    number = NumericProperty(20)
+
+class Count(Screen):
     number = NumericProperty(20)
     img = Image()
 
-    def __init__(self, **kwargs):
-        super(Count, self).__init__(**kwargs)
-        # self.padding = self.height / 5
-        self.menu()
-        # self.count_main()
-
-    def menu(self):
-        self.label = Label(text=str(self.number))
-        self.add_widget(self.label)
-        btn_cont = Button(text="Continue")
-        btn_cont.bind(on_press=self.goMain)
-        self.add_widget(btn_cont)
-        btn_reset = Button(text="Reset")
-        btn_reset.bind(on_press=self.countReset)
-        self.add_widget(btn_reset)
-
-    def count_main(self):
-        # image
-        # img = Image(source='Bubbles.jpg')
-        self.add_widget(self.img)
-
-        # counting bar: count, numbers
-        countbar = BoxLayout(size_hint=(1, .3))
-        btn = Button(text="Count It", size_hint=(.4, 1))
-        btn.bind(on_press=self.goCount)
-        countbar.add_widget(btn)
-
-        btn_menu = Button(text="Menu", size_hint=(.4, 1))
-        btn_menu.bind(on_press=self.goMenu)
-        countbar.add_widget(btn_menu)
-
-        self.label = Label(text=str(self.number), size_hint=(.2, 1))
-        countbar.add_widget(self.label)
-
-        self.add_widget(countbar)
-
-    def goMain(self, obj):
-        self.clear_widgets()
-        self.count_main()
-
-    def goMenu(self, obj):
-        self.clear_widgets()
-        self.menu()
-
-    def countReset(self, obj):
-        print("123")
-        self.number = 0
-        self.label.text = str(self.number)
-
-    def goCount(self, obj):
+    def goCount(self):
         print(123)
         self.number = self.number + 10
-        self.label.text = str(self.number)
 
 class MyApp(App):
     def build(self):
-        return Count(orientation='vertical')
+        sm = ScreenManager()
+        c = Count(name='Count')
+        sm.add_widget(c)
+        m = Menu(name='Menu')
+        sm.add_widget(m)
+        sm.current = 'Count'
+        return sm
 
+Builder.load_string("""
+<Count>:
+    BoxLayout:
+        size: root.size
+        orientation: 'vertical'
+        Image:
+            size_hint: 1, .7
+            source: 'Bubbles.jpg'
+        BoxLayout:
+            size_hint: 1, .3
+            Button:
+                size_hint: .4, 1
+                on_press: root.goCount()
+                text: 'Count It'
+            Button:
+                size_hint: .4, 1
+                on_press: root.manager.current = 'Menu'; root.manager.get_screen('Menu').number = root.number
+                # share same var by hacking
+                text: 'Menu'
+            Label:
+                size_hint: .2, 1
+                text: str(root.number)
+<Menu>:
+    BoxLayout:
+        size: root.size
+        orientation: 'vertical'
+        Label:
+            text: str(root.number)
+        Button:
+            text: 'Count It'
+            on_press: root.manager.current = 'Count'; root.manager.get_screen('Count').number = root.number
+            # share same var by hacking
+        Button:
+            text: 'Reset'
+            on_press: root.number = 0
+""")
 
 if __name__ == '__main__':
     MyApp().run()
